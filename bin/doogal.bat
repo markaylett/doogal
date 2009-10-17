aaae1b381efc620e634500b685cd601aac978a18
Mark Aylett <mark.aylett@gmail.com>
2009-10-17 22:27:50 +0100

    Added doogal jnlp.

--- bin/doogal.bat
+++ bin/doogal.bat
@@ -14,4 +14,4 @@ echo The DOOGAL_HOME environment variable is not defined correctly.
 set DOOGAL_HOME=.
 :haveDoogalHome
 
-%JAVA% -server -cp %DOOGAL_HOME%\lib\doogal-0.0.1-SNAPSHOT-jar-with-dependencies.jar org.doogal.console.Main
+%JAVA% -server -cp %DOOGAL_HOME%\lib\doogal-0.0.1-jar-with-dependencies.jar org.doogal.console.Main
