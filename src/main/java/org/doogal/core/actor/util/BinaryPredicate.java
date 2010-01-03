9b98c7289e53aafc55508056f92e25290ed688a2
Mark Aylett <mark.aylett@gmail.com>
2010-01-03 12:09:32 +0000

    Added predicates.

---
+++ src/main/java/org/doogal/core/actor/util/BinaryPredicate.java
@@ -0,0 +1,4 @@
+package org.doogal.core.actor.util;
+
+public interface BinaryPredicate<T, U> extends BinaryFunction<T, U, Boolean> {
+}
---
+++ src/main/java/org/doogal/core/actor/util/UnaryPredicate.java
@@ -0,0 +1,4 @@
+package org.doogal.core.actor.util;
+
+public interface UnaryPredicate<T> extends UnaryFunction<T, Boolean> {
+}
